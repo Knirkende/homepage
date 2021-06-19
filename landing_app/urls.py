f7a95842698dc737bc2d38006158f316be6a0ecb
Ole Holgernes <ole.holgernes@gmail.com>
2021-06-19 18:56:07 +0200

    Added basic functionality for randomized word picker.

--- landing_app/urls.py
+++ landing_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from landing_app.views import LandingPage
+from landing_app.views import LandingPageView, ibsen_view
 
 urlpatterns = [
-    path('', LandingPage.as_view(), name = 'landing_page'),
+#    path('', LandingPageView.as_view(), name = 'landing_page'),
+    path('', ibsen_view, name = 'landing_page'),
 ]
--- landing_app/views.py
+++ landing_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from landing_app.randomizer import word_picker
 
 from django.views.generic import TemplateView
 
-class LandingPage(TemplateView):
+class LandingPageView(TemplateView):
 	template_name = 'landing_app/base.html'
+
+def ibsen_view(request):
+	if request.method == 'GET':	
+		test1, test2 = word_picker()
+	return render(request, 'landing_app/base.html', {'test1': test1, 'test2': test2})
